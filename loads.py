43214371c0ddadb19434e7fe55db68875790140e
corygoates <cory.goates@gmail.com>
2023-01-31 16:45:39 -0700

    Added loads and updated gitignore for Python.

--- loads.py
+++ loads.py
@@ -72,3 +72,45 @@ class HydrostaticLoad(Load):
 
     def p(x, y):
         return self.p0*x/self.plate.a
+
+
+class PatchLoad(Load):
+    """Class for a patch load.
+    
+    Parameters
+    ----------
+    p0 : float, optional
+        Load magnitude. Defaults to 1.
+
+    c : float
+        Width of patch in x-direction.
+
+    d : float
+        Width of patch in y-direction.
+
+    x1 : float
+        Center of patch in x-direction.
+
+    x2 : float
+        Center of patch in y-direction.
+
+    plate : Plate
+        Plate on which the load is being applied.
+    """
+
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # Store patch information
+        self.c = kwargs.get("c")
+        self.d = kwargs.get("d")
+        self.x1 = kwargs.get("x1")
+        self.y1 = kwargs.get("y1")
+
+
+    def p(x, y):
+        if x < self.x1 + self.c and x > self.x1 - self.c and y < self.y1 + self.d and y > self.y1 - self.d:
+            return self.p0
+        else:
+            return 0.0
