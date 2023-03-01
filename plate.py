88f82c62c6abc6cf95127abc344c78e7bab6b2b1
Cory <cory.goates@aggiemail.usu.edu>
2023-03-01 15:20:53 -0700

    Started working on vibration of orthotropic plates.

--- plate.py
+++ plate.py
@@ -31,3 +31,83 @@ class Plate:
         self.E = kwargs.get("E", 30.0e6)
         self.v = kwargs.get("v", 0.33)
         self.D = self.E*self.h**3/(12.0*(1.0-self.v**2))
+
+
+class OrthotropicPlate:
+    """Defines an orthotropic plate for analysis using the Navier or Levy solution methods.
+    
+    Parameters
+    ----------
+    D : ndarray
+        3x3 D matrix.
+    """
+
+    def __init__(self):
+        pass
+
+
+class ZeroNinetyZeroPlate(OrthotropicPlate):
+    """A laminated plate having a 0/90/0 lamination. Material properties are assumed the same between layers.
+    
+    Parameters
+    ----------
+    a : float, optional
+        Plate dimension in the x-direction. Defaults to 1.
+
+    b : float, optional
+        Plate dimension in the y-direction. Defaults to 1.
+
+    t : float, optional
+        Lamina thickness. Defaults to 0.01.
+
+    E1 : float
+        Young's modulus in the 1-direction.
+
+    E2 : float
+        Young's modulus in the 2-direction.
+
+    v12 : float
+        Poisson's ratio.
+
+    G12 : float
+        Shear modulus.
+    """
+
+    def __init__(self, **kwargs):
+
+        # Store parameters
+        self.a = kwargs.get("a", 1)
+        self.b = kwargs.get("b", 1)
+        self.t = kwargs.get("t", 0.01)
+        self.h = self.t*3.0
+        self.E1 = kwargs.get("E1")
+        self.E2 = kwargs.get("E2")
+        self.v12 = kwargs.get("v12")
+        self.G12 = kwargs.get("G12")
+
+        # Calculate derived properties
+        self.v21 = self.E2*self.v12/self.E1
+        self.calc_D()
+
+    
+    def calc_D(self):
+        # Calculates the elements of the D matrix
+
+        # Get Q matrices
+        denom = 1.0 - self.v12*self.v21
+        Q11_TB = self.E1/denom
+        Q22_TB = self.E2/denom
+        Q12_TB = self.v21*self.E1/denom
+        Q66_TB = self.G12
+        Q11_mid = Q22_TB
+        Q22_mid = Q11_TB
+        Q12_mid = Q12_TB
+        Q66_mid = Q66_TB
+
+        # Get D
+        f_TB = 2.0*((0.5*self.h)**3 - (0.5*self.t)**3)/3.0
+        f_mid = 2.0*(0.5*self.t)**3/3.0
+        self.D11 = f_TB*Q11_TB + f_mid*Q11_mid
+        self.D22 = f_TB*Q22_TB + f_mid*Q22_mid
+        self.D12 = f_TB*Q12_TB + f_mid*Q12_mid
+        self.D66 = f_TB*Q66_TB + f_mid*Q66_mid
---
+++ project1.py
@@ -0,0 +1,141 @@
+import sys
+
+import matplotlib.pyplot as plt
+
+from plate import Plate
+from loads import *
+from solver import NavierSolver, LevySolver
+
+
+def load_input(input_file):
+    # Loads the input and returns the solver object
+
+    # Get input
+    with open(input_file) as input_handle:
+        input_lines = input_handle.readlines()
+
+    # Geometry
+    a = float(input_lines[0].split()[-1])
+    b = float(input_lines[1].split()[-1])
+    h = float(input_lines[2].split()[-1])
+
+    print()
+    print("---Plate Dimensions---")
+    print()
+    print("a:", a)
+    print("b:", b)
+    print("h:", h)
+
+    # Material properties
+    E = float(input_lines[3].split()[-1])
+    v = float(input_lines[4].split()[-1])
+    print()
+    print("---Material Properties---")
+    print()
+    print("E:", E)
+    print("v:", v)
+
+    # Initialize plate
+    plate = Plate(a=a, b=b, h=h, E=E, v=v)
+    print("D:", plate.D)
+
+    # Locations of interest
+    x0 = input_lines[5].split('=')[-1].strip()[1:-1]
+    x0 = [float(x.strip()) for x in x0.split(',')]
+    y0 = input_lines[6].split('=')[-1].strip()[1:-1]
+    y0 = [float(y.strip()) for y in y0.split(',')]
+
+    # Load
+    load_type = int(input_lines[7].split()[-1])
+    p0 = float(input_lines[8].split()[-1])
+    print()
+    print("---Load---")
+    print()
+    if load_type == 0:
+        load = UniformLoad(p0=p0, plate=plate)
+        print("Type: uniform")
+    elif load_type == 1:
+        load = SinusoidalLoad(p0=p0, plate=plate)
+        print("Type: sinusoidal")
+    elif load_type == 2:
+        load = HydrostaticLoad(p0=p0, plate=plate)
+        print("Type: hydrostatic")
+    elif load_type == 3:
+        c = float(input_lines[9].split()[-1])
+        d = float(input_lines[10].split()[-1])
+        x = float(input_lines[11].split()[-1])
+        y = float(input_lines[12].split()[-1])
+        load = PatchLoad(p0=p0, c=c, d=d, x=x, y=y)
+        print("Type: patch")
+    print("P0: ", p0)
+
+    # Solver parameters
+    m_max = int(input_lines[-4].split()[-1])
+    n_max = int(input_lines[-3].split()[-1])
+    solver = input_lines[-2].split()[-1]
+    BC = input_lines[-1].split()[-1]
+
+    print()
+    print("---Solver---")
+    print()
+    print("Type: ", solver.title())
+    print("Boundary Conditions: ", BC)
+    print("m_max: ", m_max)
+
+    if solver == "navier":
+        if BC != 'SSSS':
+            raise IOError("Navier solver cannot be used with BCs other than SSSS! Quitting...")
+        solver = NavierSolver(plate, load, m_max, n_max)
+        print("n_max: ", n_max)
+    else:
+        if BC[0] != 'S' or BC[2] != 'S':
+            raise IOError("Levy solver must have 'S' BCs on x-faces! Quitting...")
+        solver = LevySolver(plate, load, BC, m_max)
+        print("Symmetric: ", solver.symmetric)
+
+    return solver
+
+
+if __name__=="__main__":
+
+    # Load input
+    input_file = sys.argv[-1]
+    solver = load_input(input_file)
+
+    # Print stresses
+    print()
+    print("---Solution---")
+    print()
+    print("Bending stresses at center")
+    print("    \u03C3_x: {0:.3f}".format(solver.sigma_x(0.5*solver.plate.a, 0.5*solver.plate.b, 0.5*solver.plate.h)))
+    print("    \u03C3_y: {0:.3f}".format(solver.sigma_y(0.5*solver.plate.a, 0.5*solver.plate.b, 0.5*solver.plate.h)))
+    print()
+    print("Shear stresses at corners")
+    print("(0,0):")
+    print("    \u03C4_xy: {0:.3f}".format(solver.tau_xy(0.0, 0.0, 0.5*solver.plate.h)))
+    print("(a,0):")
+    print("    \u03C4_xy: {0:.3f}".format(solver.tau_xy(solver.plate.a, 0.0, 0.5*solver.plate.h)))
+    print("(a,b):")
+    print("    \u03C4_xy: {0:.3f}".format(solver.tau_xy(solver.plate.a, solver.plate.b, 0.5*solver.plate.h)))
+    print("(0,b):")
+    print("    \u03C4_xy: {0:.3f}".format(solver.tau_xy(0.0, solver.plate.b, 0.5*solver.plate.h)))
+
+    # Get max deflection
+    x_w_max, y_w_max, w_max = solver.get_maximum_w()
+    print()
+    print("Deformations")
+    print("    Maximum: {0:.5f} at ({1:.5f}, {2:.5f})".format(w_max, x_w_max, y_w_max))
+    print("    At center: {0:.5f} at ({1:.1f}, {2:.1f})".format(solver.w(0.5*solver.plate.a, 0.5*solver.plate.b), 0.5*solver.plate.a, 0.5*solver.plate.b))
+
+    print()
+    print("Average corner reactions")
+    print("    {0:.2f} per corner".format(solver.corner_reactions()))
+
+    print()
+    print("Maximum stresses")
+    print("    \u03C3_x: {3:.3f} at ({0:.2f}, {1:.2f}, {2:.2f})".format(*solver.get_maximum_sigma_x()))
+    print("    \u03C3_y: {3:.3f} at ({0:.2f}, {1:.2f}, {2:.2f})".format(*solver.get_maximum_sigma_y()))
+    print("    \u03C4_xz: {3:.3f} at ({0:.2f}, {1:.2f}, {2:.2f})".format(*solver.get_maximum_tau_xz()))
+    print("    \u03C4_yz: {3:.3f} at ({0:.2f}, {1:.2f}, {2:.2f})".format(*solver.get_maximum_tau_yz()))
+
+    #solver.plot_deflection_field()
